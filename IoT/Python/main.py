9005445eeec19d27d5821076690374bf9c3109e1
Melvin <melvin.bouyssou@gmail.com>
2024-11-27 11:57:38 +0100

    [IoT] Python : ajout de la gestion de fréquence, fichier cache

--- IoT/Python/main.py
+++ IoT/Python/main.py
@@ -2,7 +2,6 @@ import json
 import threading
 import paho.mqtt.subscribe as subscribe
 from datetime import datetime
-import time
 import os
 
 server = str()
@@ -13,29 +12,35 @@ frequence = int()
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+'''
+Fonction qui vérifie l'existence des fichiers de résultat, de cache et d'alerte et les crée s'ils n'existent pas.
+'''
 def verifFichier():
     fichiers = {
         "../resultat/resultatAM107.json": {},
         "../resultat/resultatSolar.json": {},
         "../resultat/alerteAM107.json": {},
+        "cache/cache_am107.json": {},
+        "cache/cache_solaredge.json": {}
     }
     for fichier, contenu in fichiers.items():
         try:
-            with open(fichier, 'r') as file:
+            with open(fichier, 'r', encoding='utf-8') as file:
                 json.load(file)
         except FileNotFoundError:
-            with open(fichier, 'w') as file:
-                json.dump(contenu, file, indent=4)
+            with open(fichier, 'w', encoding='utf-8') as file:
+                json.dump(contenu, file, ensure_ascii=False, indent=4)
                 print(f"Fichier {fichier} créé")
 
 
+'''
+Fonction qui charge la configuration depuis le fichier config.json et initialise les variables globales.
+Cette fonction crée un fichier de configuration par défaut si le fichier config.json n'existe pas.
+'''
 def config():
-    """
-    Charge la configuration depuis le fichier JSON et initialise les variables globales.
-    """
     print("Chargement de la config...")
     try:
-        with open("../config.json", 'r') as file:
+        with open("../config.json", 'r', encoding='utf-8') as file:
             data = json.load(file)
             global server, topic, salle, dataVoulu, frequence
             topic = [{"type": "AM107", "topic": t} if "AM107" in t else {"type": "SolarEdge", "topic": t} for t in data.get("topic")]
@@ -49,7 +54,7 @@ def config():
         # Créer un fichier de configuration par défaut
         print(f"Erreur lors du chargement de la config : {e}")
         print("Création d'un fichier de configuration par défaut...")
-        with open("../config.json", 'w') as file:
+        with open("../config.json", 'w', encoding='utf-8') as file:
             data = {
                 "server": "mqtt.iut-blagnac.fr",
                 "topic" : ["AM107/by-room/#", "solaredge/blagnac/#"],
@@ -57,7 +62,7 @@ def config():
                 "data": {"temperature": 30, "humidity": 30, "co2": 1800, "illumination": 20},
                 "frequence": 10
             }
-            json.dump(data, file, indent=4)
+            json.dump(data, file, ensure_ascii=False, indent=4)
         print("Fichier de configuration par défaut créé avec succès")
 
 '''                
@@ -69,24 +74,24 @@ def on_message_print(type, message):
         if not payload:
             raise ValueError("Le payload est vide")
         data = json.loads(payload)
-        print(data)  # debug
-
         try:
             if type == "AM107":  # Traitement des données AM107
                 # Traitement des données AM107
-                traitement_am107(data)
+                ecrireCacheAm107(data, "cache/cache_am107.json")
             elif type == "SolarEdge":
                 # Traitement des données SolarEdge
-                traitement_solaredge(data)
+                ecrireCacheSolaredge(data, "cache/cache_solaredge.json")
         except Exception as e:
             print(f"Erreur dans le traitement des données: {e}")
     except Exception as e:
         print(f"Erreur dans la réception du message : {e}")
 
 
-
+'''
+Fonction qui ajoute une alerte dans le fichier d'alerte en cas de dépassement de seuil. (Ne prend pas en compte la fréquence = ajoute directement au fichier d'alerte)
+'''
 def alerteAM107(room, capteur, valeur):
-    with open("../resultat/alerteAM107.json", "r+") as file:
+    with open("../resultat/alerteAM107.json", "r+", encoding='utf-8') as file:
         try:
             content = file.read()
             if content:
@@ -102,19 +107,27 @@ def alerteAM107(room, capteur, valeur):
 
             if capteur not in fichierJson[room]:
                 fichierJson[room][capteur] = []
-
+            
+            if "timestamp" not in fichierJson[room]:
+                fichierJson[room]["timestamp"] = []
+                
             fichierJson[room][capteur].append(valeur)
-
+            if datetime.now().strftime("%Y-%m-%d %H:%M:%S") not in fichierJson[room]["timestamp"]:
+                fichierJson[room]["timestamp"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            
         except Exception as e:
             print(f"Erreur dans la récupération des données: {e}")
             return
 
         file.seek(0)
-        file.write(json.dumps(fichierJson, indent=4))
+        file.write(json.dumps(fichierJson, ensure_ascii=False, indent=4))
         file.truncate()
 
-def traitement_am107(data):
-    with open("../resultat/resultatAM107.json", "r+") as file:
+'''
+Fonction qui écrit les données dans le fichier cache.
+'''
+def ecrireCacheAm107(data: dict, fichier: str):
+    with open(fichier, "r+", encoding='utf-8') as file:
         try:
             content = file.read()
             if content:
@@ -135,7 +148,7 @@ def traitement_am107(data):
             # Arreter le traitement si la salle n'est pas dans la liste des salles à traiter
             if room not in salle and "all" not in salle:
                 return
-
+            
             # Initialiser la salle dans le fichier JSON si elle n'existe pas
             if room not in fichierJson:
                 fichierJson[room] = {}
@@ -162,16 +175,15 @@ def traitement_am107(data):
             return
 
         file.seek(0)
-        file.write(json.dumps(fichierJson, indent=4))
+        file.write(json.dumps(fichierJson, ensure_ascii=False, indent=4))
         file.truncate()
-
-
+        
 '''
-Fonction qui traite les données SolarEdge.
+Fonction qui écrit les données dans le fichier cache.
 '''
-def traitement_solaredge(data):
+def ecrireCacheSolaredge(data: dict, fichier: str):
     # Charger le fichier JSON existant
-    with open("../resultat/resultatSolar.json", "r+") as file:
+    with open(fichier, "r+", encoding='utf-8') as file:
         try:
             content = file.read()
             fichierJson = json.loads(content) if content else {}
@@ -225,30 +237,141 @@ def traitement_solaredge(data):
 
         # Sauvegarder dans le fichier JSON
         file.seek(0)
-        file.write(json.dumps(fichierJson, indent=4))
+        file.write(json.dumps(fichierJson, ensure_ascii=False, indent=4))
+        file.truncate()
+        
+'''
+Fontion qui ajoute les données du cache dans le fichier de résultat.
+'''
+def appendCacheToResultatAm107(fichierCache: str, fichierResultat: str):
+    with open(fichierCache, "r", encoding='utf-8') as file:
+        try:
+            content = file.read()
+            if content:
+                fichierJson = json.loads(content)
+            else:
+                return
+        except json.JSONDecodeError:
+            return
+
+    with open(fichierResultat, "r+", encoding='utf-8') as file:
+        try:
+            content = file.read()
+            if content:
+                resultatJson = json.loads(content)
+            else:
+                resultatJson = {}
+        except json.JSONDecodeError:
+            resultatJson = {}
+
+        try:
+            for room, data in fichierJson.items():
+                if room not in resultatJson:
+                    resultatJson[room] = data
+                else:
+                    for capteur, valeurs in data.items():
+                        if capteur not in resultatJson[room]:
+                            resultatJson[room][capteur] = valeurs
+                        else:
+                            resultatJson[room][capteur] += valeurs
+
+        except Exception as e:
+            print(f"Erreur dans la récupération des données: {e}")
+            return
+
+        file.seek(0)
+        file.write(json.dumps(resultatJson, ensure_ascii=False, indent=4))
+        file.truncate()
+        
+    # Vider le fichier cache
+    with open(fichierCache, "w", encoding='utf-8') as file:
+        file.write("{}")
+        
+'''
+Fontion qui ajoute les données du cache dans le fichier de résultat.
+'''
+def appendCacheToResultatSolaredge(fichierCache: str, fichierResultat: str):
+    with open(fichierCache, "r", encoding='utf-8') as file:
+        try:
+            content = file.read()
+            if content:
+                fichierJson = json.loads(content)
+            else:
+                return
+        except json.JSONDecodeError:
+            return
+
+    with open(fichierResultat, "r+", encoding='utf-8') as file:
+        try:
+            content = file.read()
+            if content:
+                resultatJson = json.loads(content)
+            else:
+                resultatJson = {}
+        except json.JSONDecodeError:
+            resultatJson = {}
+
+        try:
+            # Si le fichier est vide, on copie son contenu dans resultatJson
+            if not resultatJson:
+                resultatJson = fichierJson
+            else:
+                # Vérifie si "lastUpdateTime" existe et contient de nouveaux timestamps
+                if "lastUpdateTime" in fichierJson:
+                    for timestamp in fichierJson["lastUpdateTime"]:
+                        if timestamp not in resultatJson["lastUpdateTime"]:
+                            resultatJson["lastUpdateTime"].append(timestamp)
+                            # Ajout des autres valeurs associées à ce timestamp
+                            for key in fichierJson:
+                                if key == "lastUpdateTime":
+                                    continue
+                                if key not in resultatJson:
+                                    resultatJson[key] = []
+                                if key in fichierJson:
+                                    index = fichierJson["lastUpdateTime"].index(timestamp)
+                                    resultatJson[key].append(fichierJson[key][index])
+                                else:
+                                    resultatJson[key].append(None)
+                                    
+        except Exception as e:
+            print(f"Erreur dans la récupération des données: {e}")
+            return
+
+        file.seek(0)
+        file.write(json.dumps(resultatJson, ensure_ascii=False, indent=4))
         file.truncate()
 
-
-
+    # Vider le fichier cache
+    with open(fichierCache, "w", encoding='utf-8') as file:
+        file.write("{}")
+       
+'''
+Fonction qui écrit les données dans le fichier de résultat.
+C'est ici que l'on relance le timer pour la prochaine écriture. (fréquence = temps entre chaque écriture)
+''' 
+def ecriture():
+    print("Transfert ...")
+    appendCacheToResultatAm107("cache/cache_am107.json", "../resultat/resultatAM107.json")
+    appendCacheToResultatSolaredge("cache/cache_solaredge.json", "../resultat/resultatSolar.json")
+    timer = threading.Timer(frequence, ecriture)
+    timer.start()
 
 '''
 Fonction qui permet de s'abonner à un topic et de traiter les messages reçus.
 '''
 def thread_subscribe(type, topic):
     subscribe.callback(lambda client, userdata, message: on_message_print(type, message), topic, hostname=server)
-    #time.sleep(frequence)
 
 # Initialisation
 verifFichier()
 config()
 
-# Création des threads pour chaques topic et comportement attendu
+# Création des threads pour chaques topic et comportement attendu (on a donc un thread par topic soit 2 threads), un thread pour l'écriture des données et un timer pour la fréquence
 threads = []
+timer = threading.Timer(frequence, ecriture)
+print(f"timer start {frequence}")
+timer.start()
 for elt in topic:
     thread = threading.Thread(target=thread_subscribe, args=(elt["type"], elt["topic"]))
     threads.append(thread)
     thread.start()
-
-# Attente de la fin de tous les threads (optionnel)
-for thread in threads:
-    thread.join()
